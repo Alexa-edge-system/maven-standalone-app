257d33fb81558c38263c5833f6e651d90fdb05fc
Titalearn <126238347+Titalearn@users.noreply.github.com>
2025-05-29 18:49:08 +0200

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Standalone App!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
